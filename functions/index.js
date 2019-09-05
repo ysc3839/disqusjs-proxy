cc9e20233757290ae2ec3f534ef99998b84054bf
Richard Yu <yurichard3839@gmail.com>
2019-09-05 14:04:27 +0800

    Move main function to master branch

--- functions/index.js
+++ functions/index.js
@@ -1,41 +1,5 @@
 const functions = require('firebase-functions');
-const URL = require('url').URL;
+const api = require('disqusjs-proxy');
 const config = require('./config.json');
 
-const option = {
-  target: 'https://disqus.com/api/',
-  secure: true,
-  changeOrigin: true,
-  ...config.proxyOpt
-};
-
-if (config.resHeaders) {
-  option.onProxyRes = (proxyRes, req, res) => {
-    if (proxyRes.statusCode === 200)
-      Object.assign(proxyRes.headers, config.resHeaders);
-  };
-}
-
-const proxy = require('http-proxy-middleware')(option);
-
-exports.disqus = functions.https.onRequest((req, res) => {
-  if (config.refererHost) {
-    var pass = false;
-    try {
-      const hostname = new URL(req.header('referer')).hostname;
-      if (typeof config.refererHost === 'string') {
-        pass = (hostname === config.refererHost);
-      } else if (config.refererHost instanceof Array) {
-        pass = config.refererHost.includes(hostname);
-      }
-    } catch (e) {}
-    if (!pass) {
-      res.status(404).set('vary', 'Referer')
-        .set('cache-control', 'public, max-age=604800, s-maxage=604800')
-        .end();
-      return;
-    }
-  }
-
-  proxy(req, res);
-});
+exports.disqus = functions.https.onRequest(api(config));
